fa38c5f15fa6cc446b88b53a69dd1d3cf73f42e4
Manuel Munz <munz@comuno.net>
2022-10-30 17:40:27 +0100

    Fix class mapping

--- Configuration/Extbase/Persistence/Classes.php
+++ Configuration/Extbase/Persistence/Classes.php
@@ -2,13 +2,13 @@
 declare(strict_types = 1);
 
 return [
-    \GeorgRinger\News\Domain\Model\News::class => [
+    GeorgRinger\News\Domain\Model\News::class => [
         'tableName' => 'tx_news_domain_model_news',
         'subclasses' => [
             0 => \C1\NewsSinglepage\Domain\Model\NewsDefault::class
         ]
     ],
-    \C1\NewsSinglepage\Domain\Model\NewsDefault::class => [
+    C1\NewsSinglepage\Domain\Model\NewsDefault::class => [
         'tableName' => 'tx_news_domain_model_news',
         'recordType' => 0,
     ],
